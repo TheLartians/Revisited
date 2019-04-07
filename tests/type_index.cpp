5cab5c589e0c9c6b4aab727f24440a3ec23465eb
Lars Melchior <TheLartians@users.noreply.github.com>
2019-04-07 16:11:07 +0200

    convert static_assert to require for gcc

--- tests/type_index.cpp
+++ tests/type_index.cpp
@@ -5,17 +5,17 @@ TEST_CASE("Type Index") {
 
   using namespace lars;
 
-  static_assert(get_type_index<int>() == get_type_index<int>());
-  static_assert(get_type_index<float>() == get_type_index<float>());
-  static_assert(get_type_index<int>() != get_type_index<float>());
+  REQUIRE(get_type_index<int>() == get_type_index<int>());
+  REQUIRE(get_type_index<float>() == get_type_index<float>());
+  REQUIRE(get_type_index<int>() != get_type_index<float>());
 
   using A = int;
   using B = float;
 
-  static_assert(get_type_index<A>() == get_type_index<int>());
-  static_assert(get_type_index<B>() == get_type_index<float>());
-  static_assert(get_type_index<A>() != get_type_index<B>());
-  static_assert(get_type_index<A>() != get_type_index<float>());
-  static_assert(get_type_index<B>() != get_type_index<int>());
+  REQUIRE(get_type_index<A>() == get_type_index<int>());
+  REQUIRE(get_type_index<B>() == get_type_index<float>());
+  REQUIRE(get_type_index<A>() != get_type_index<B>());
+  REQUIRE(get_type_index<A>() != get_type_index<float>());
+  REQUIRE(get_type_index<B>() != get_type_index<int>());
   
 }
