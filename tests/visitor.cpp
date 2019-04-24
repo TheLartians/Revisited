cbce31dc864df7f7585b91d74907750806755d1a
Lars Melchior <TheLartians@users.noreply.github.com>
2019-04-24 13:43:12 +0200

    add recusive visitor test

--- tests/visitor.cpp
+++ tests/visitor.cpp
@@ -28,6 +28,9 @@ namespace {
   struct BX: public DerivedVisitable<BX, JointVisitable<B, X>> {
   };
   
+  struct XB: public DerivedVisitable<XB, JointVisitable<X, B>> {
+  };
+
   struct ABCVisitor: public lars::Visitor<const A, const B, const C> {
     char result = 0;
     
@@ -51,37 +54,72 @@ namespace {
       v.accept(*this);
       return result;
     }
-    
   };
   
-  struct ABXVisitor: public lars::RecursiveVisitor<A, B, X> {
-    std::string result;
-    
-    bool visit(A &v) override {
+  struct ABXVisitor: public lars::Visitor<A, B, X> {
+    char result = 0;
+
+    void visit(A &v) override {
       result += v.name;
-      return true;
     }
     
-    bool visit(B &v) override {
+    void visit(B &v) override {
       result += v.name;
-      return true;
     }
     
-    bool visit(X &) override {
+    void visit(X &) override {
       result += 'X';
-      return true;
     }
     
-    auto getTypeName(VisitableBase &v) {
-      result = "";
+    char getTypeName(VisitableBase &v) {
+      result = 0;
       v.accept(*this);
       return result;
     }
-    
   };
+  
+  struct ABCXRecursiveVisitor: public lars::RecursiveVisitor<const A, const B, const C, const X> {
+    std::string result;
+    bool recursive;
+
+    bool visit(const A &v) override {
+      result += v.name;
+      return recursive;
+    }
+    
+    bool visit(const B &v) override {
+      result += v.name;
+      return recursive;
+    }
+    
+    bool visit(const C &v) override {
+      result += v.name;
+      return recursive;
+    }
+    
+    bool visit(const X &) override {
+      result += 'X';
+      return recursive;
+    }
+    
+    char getTypeName(const VisitableBase &v) {
+      result = "";
+      recursive = false;
+      v.accept(*this);
+      return result[0];
+    }
+    
+    std::string getFullTypeName(const VisitableBase &v) {
+      result = "";
+      recursive = true;
+      v.accept(*this);
+      return result;
+    }
+  };
+
 }
 
-TEST_CASE("New") {
+TEST_CASE("Visitor") {
   using namespace lars;
   
   std::shared_ptr<VisitableBase> a = std::make_shared<A>();
@@ -90,24 +128,55 @@ TEST_CASE("New") {
   std::shared_ptr<VisitableBase> x = std::make_shared<X>();
   std::shared_ptr<VisitableBase> cx = std::make_shared<CX>();
   std::shared_ptr<VisitableBase> bx = std::make_shared<BX>();
+  std::shared_ptr<VisitableBase> xb = std::make_shared<XB>();
+
+  SECTION("ABCVisitor"){
+    ABCVisitor visitor;
+    REQUIRE(visitor.asVisitorFor<const A>() == static_cast<SingleVisitor<const A>*>(&visitor));
+    REQUIRE(visitor.asVisitorFor<const B>() == static_cast<SingleVisitor<const B>*>(&visitor));
+    REQUIRE(visitor.asVisitorFor<const C>() == static_cast<SingleVisitor<const C>*>(&visitor));
+    REQUIRE(visitor.asVisitorFor<X>() == nullptr);
+    
+    REQUIRE(visitor.getTypeName(*a) == 'A');
+    REQUIRE(visitor.getTypeName(*b) == 'B');
+    REQUIRE(visitor.getTypeName(*c) == 'C');
+    REQUIRE_THROWS_AS(visitor.getTypeName(*x), IncompatibleVisitorException);
+    REQUIRE_THROWS_WITH(visitor.getTypeName(*x), Catch::Matchers::Contains("X") && Catch::Matchers::Contains("incompatible visitor"));
+    REQUIRE(visitor.getTypeName(*cx) == 'C');
+    REQUIRE(visitor.getTypeName(*bx) == 'B');
+    REQUIRE(visitor.getTypeName(*xb) == 'B');
+ }
   
-  ABCVisitor abcVisitor;
-  ABXVisitor abxVisitor;
+  SECTION("ABXVisitor"){
+    ABXVisitor visitor;
+    
+    REQUIRE(visitor.getTypeName(*a) == 'A');
+    REQUIRE(visitor.getTypeName(*b) == 'B');
+    REQUIRE(visitor.getTypeName(*c) == 'A');
+    REQUIRE(visitor.getTypeName(*x) == 'X');
+    REQUIRE(visitor.getTypeName(*cx) == 'A');
+    REQUIRE(visitor.getTypeName(*bx) == 'B');
+    REQUIRE(visitor.getTypeName(*xb) == 'X');
+  }
   
-  REQUIRE(abcVisitor.asVisitorFor<const A>() == static_cast<SingleVisitor<const A>*>(&abcVisitor));
-  REQUIRE(abcVisitor.asVisitorFor<const B>() == static_cast<SingleVisitor<const B>*>(&abcVisitor));
-  REQUIRE(abcVisitor.asVisitorFor<const C>() == static_cast<SingleVisitor<const C>*>(&abcVisitor));
-  REQUIRE(abcVisitor.asVisitorFor<X>() == nullptr);
+  SECTION("ABCXRecursiveVisitor"){
+    ABCXRecursiveVisitor visitor;
+
+    REQUIRE(visitor.getTypeName(*a) == 'A');
+    REQUIRE(visitor.getTypeName(*b) == 'B');
+    REQUIRE(visitor.getTypeName(*c) == 'C');
+    REQUIRE(visitor.getTypeName(*x) == 'X');
+    REQUIRE(visitor.getTypeName(*cx) == 'C');
+    REQUIRE(visitor.getTypeName(*bx) == 'B');
+    REQUIRE(visitor.getTypeName(*xb) == 'X');
+
+    REQUIRE(visitor.getFullTypeName(*a) == "A");
+    REQUIRE(visitor.getFullTypeName(*b) == "B");
+    REQUIRE(visitor.getFullTypeName(*c) == "CA");
+    REQUIRE(visitor.getFullTypeName(*x) == "X");
+    REQUIRE(visitor.getFullTypeName(*cx) == "CAX");
+    REQUIRE(visitor.getFullTypeName(*bx) == "BX");
+    REQUIRE(visitor.getFullTypeName(*xb) == "XB");
+  }
   
-  REQUIRE(abcVisitor.getTypeName(*a) == 'A');
-  REQUIRE(abcVisitor.getTypeName(*b) == 'B');
-  REQUIRE(abcVisitor.getTypeName(*c) == 'C');
-  
-  REQUIRE_THROWS_AS(abcVisitor.getTypeName(*x), IncompatibleVisitorException);
-  REQUIRE_THROWS_WITH(abcVisitor.getTypeName(*x), Catch::Matchers::Contains("X") && Catch::Matchers::Contains("incompatible visitor"));
-  REQUIRE(abxVisitor.getTypeName(*x) == "X");
-  
-  REQUIRE(abcVisitor.getTypeName(*cx) == 'C');
-  REQUIRE(abxVisitor.getTypeName(*cx) == "AX");
-  REQUIRE(abxVisitor.getTypeName(*bx) == "BX");
 }
