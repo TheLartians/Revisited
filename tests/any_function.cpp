eb90420e005002659b4ddc7ed676776e0b89d058
Lars Melchior <TheLartians@users.noreply.github.com>
2019-04-29 17:08:43 +0200

    Allow implicit casting of Visitable types in AnyFunctions (#35)
    * Allow implicit casting of Visitable types in AnyFunctions
    
    * update tests

--- tests/any_function.cpp
+++ tests/any_function.cpp
@@ -117,3 +117,14 @@ TEST_CASE("any_with_any_function","[any_function][any]"){
   Any af = f;
   REQUIRE(af.get<const AnyFunction &>()().get<int>() == 42);
 }
+
+TEST_CASE("Automatic Casting"){
+  struct A: public lars::Visitable<A> { int value = 0; };
+  struct B: public lars::DerivedVisitable<B, A> { B(){ value = 1; } };
+  struct C: public lars::DerivedVisitable<C, B> { C(){ value = 2; } };
+
+  AnyFunction f  = [](A & x,A & y){ A a; a.value = x.value+y.value; return a; };
+  REQUIRE_THROWS(f(B(),C()).get<A>().value);  // drawback with visitable types: cannot be captured by value
+  REQUIRE(f(B(),C()).get<A &>().value == 3);
+  REQUIRE(f(B(),C()).get<const A &>().value == 3);
+}
