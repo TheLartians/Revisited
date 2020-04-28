969f14a748f15190231b9d9d159df14e47f8c3a1
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-28 19:03:01 +0200

    use type index instead of type id (#57)
    * use typeindex istead of typeid
    
    * push version
    
    * restrict workflows to master branch
    
    * fix format

--- include/revisited/type_index.h
+++ include/revisited/type_index.h
@@ -5,11 +5,13 @@
 #include <functional>
 
 namespace revisited {
-  using TypeIndex = static_type_info::TypeID;
+  using TypeIndex = static_type_info::TypeIndex;
 
   struct TypeID : public static_type_info::TypeID {};
 
-  template <class T> constexpr TypeIndex getTypeIndex() { return static_type_info::getTypeID<T>(); }
+  template <class T> constexpr TypeIndex getTypeIndex() {
+    return static_type_info::getTypeIndex<T>();
+  }
 
   template <class T> TypeID getTypeID() { return TypeID{static_type_info::getTypeID<T>()}; }
 
